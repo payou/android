6ea1068cddb577561c62d8d10afab131731093e2
unknown <olivierandrejc@gmail.com>
2014-05-31 08:20:30 +0200

    commit

---
+++ gen/edu/vanderbilt/mooc/mooc_cp/Manifest.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package edu.vanderbilt.mooc.mooc_cp;
+
+/* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
+public final class Manifest {
+}
